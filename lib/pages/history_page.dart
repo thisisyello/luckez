2c3c7374c9e2724a9b35994e7d9fd7df359c7b11
yello <yello.3617@gmail.com>
2026-08-12 19:19:55 +0900

    역대 당첨번호 페이지 구현 및 불필요한 코드 제거

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,374 +1,108 @@
-// import 'package:flutter/material.dart';
-// import 'package:randomlottonumber/style.dart';
-// import 'package:randomlottonumber/num_history_lists.dart';
+import 'package:flutter/material.dart';
+import 'package:randomlottonumber/data/num_history_lists.dart';
+import 'package:randomlottonumber/style.dart';
 
-// class AboutWinningNumPage extends StatelessWidget {
-//   const AboutWinningNumPage({super.key});
+class HistoryPage extends StatelessWidget {
+  const HistoryPage({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: 2,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: mainColor,
-//           iconTheme: IconThemeData(
-//             color: whiteColor
-//           ),
-//           title: Text(
-//             '당첨번호',
-//             style: TextStyle(
-//               fontWeight: FontWeight.w700,
-//               color: whiteColor,
-//             ),
-//           ),
-//           bottom: TabBar(
-//             tabs: <Widget> [
-//               Tab(
-//                 child: Text(
-//                   '역대 당첨번호',
-//                   style: TextStyle(
-//                     color: whiteColor,
-//                   ),
-//                 ),
-//               ),
-//               Tab(
-//                 child: Text(
-//                   '당첨번호 출현 순위',
-//                   style: TextStyle(
-//                     color: whiteColor,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//             indicatorColor: whiteColor,
-//           ),
-//         ),
-//         body: const TabBarView(
-//           children: <Widget> [
-//             WinningNumScreen(),
-//             DuplicateNumsScreen(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    final reversedList = historyNumberLists.reversed.toList();
 
-// // ------------------------------------- 역대 당첨번호 -------------------------------------
-// class WinningNumScreen extends StatefulWidget {
-//   const WinningNumScreen({super.key});
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '역대 당첨번호',
+          style: TextStyle(color: blackColor),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: blackColor),
+      ),
+      backgroundColor: whiteColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ListView.builder(
+          itemCount: reversedList.length,
+          itemBuilder: (context, index) {
+            final numbers = reversedList[index].sublist(0, 6);
+            final bonusNumber = reversedList[index][6];
 
-//   @override
-//   State<WinningNumScreen> createState() => _WinningNumScreenState();
-// }
+            return Container(
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    spreadRadius: 1,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '제 ${historyNumberLists.length - index} 회 당첨번호',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: blackColor,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ...numbers.map(
+                        (number) => HistoryNumberBall(
+                          number: number,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: greyColor,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      HistoryNumberBall(number: bonusNumber),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
 
-// class _WinningNumScreenState extends State<WinningNumScreen> {
-//   var historyListsLength = historyNumberLists.length;
-//   var reverseHistory = historyNumberLists.reversed.toList();
-//   var reverseBonus = bonusNumbers.reversed.toList();
+class HistoryNumberBall extends StatelessWidget {
+  const HistoryNumberBall({
+    super.key,
+    required this.number,
+  });
 
-//   int listCount = 20;
+  final int number;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.symmetric(horizontal: 16),
-//       width: MediaQuery.of(context).size.width,
-//       height: MediaQuery.of(context).size.height,
-//       child: Center(
-//         child: ConstrainedBox(
-//           constraints: const BoxConstraints(
-//             maxWidth: 500,
-//           ),
-//           child: ListView.builder(
-//             itemCount: historyListsLength,
-//             itemBuilder: (context, index) {
-//               return Container(
-//                 height: 56,
-//                 // margin: const EdgeInsets.symmetric(vertical: 8),
-//                 decoration: BoxDecoration(
-//                   border: Border(
-//                     bottom: BorderSide(
-//                       width: 1,
-//                       color: greyColor,
-//                     )
-//                   )
-//                 ),
-//                 child: Row(
-//                   children: [
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(horizontal: 4),
-//                       child: Container(
-//                         padding: const EdgeInsets.only(right: 8),
-//                         width: 64,
-//                         decoration: BoxDecoration(
-//                           border: Border(
-//                             right: BorderSide(
-//                               width: 2,
-//                               color: greyColor,
-//                             )
-//                           )
-//                         ),
-//                         child: Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: [
-//                             Text(
-//                               '${historyListsLength - index}',
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 color: blackColor,
-//                               ),
-//                             ),
-//                             Text(
-//                               '회',
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 color: blackColor,
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][0]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][0] < 11 ? num0Color : reverseHistory[index][0] < 21 ? num10Color : reverseHistory[index][0] < 31 ? num20Color  : reverseHistory[index][0] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][1]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][1] < 11 ? num0Color : reverseHistory[index][1] < 21 ? num10Color : reverseHistory[index][1] < 31 ? num20Color  : reverseHistory[index][1] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][2]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][2] < 11 ? num0Color : reverseHistory[index][2] < 21 ? num10Color : reverseHistory[index][2] < 31 ? num20Color  : reverseHistory[index][2] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][3]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][3] < 11 ? num0Color : reverseHistory[index][3] < 21 ? num10Color : reverseHistory[index][3] < 31 ? num20Color  : reverseHistory[index][3] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][4]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][4] < 11 ? num0Color : reverseHistory[index][4] < 21 ? num10Color : reverseHistory[index][4] < 31 ? num20Color  : reverseHistory[index][4] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseHistory[index][5]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseHistory[index][5] < 11 ? num0Color : reverseHistory[index][5] < 21 ? num10Color : reverseHistory[index][5] < 31 ? num20Color  : reverseHistory[index][5] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '+',
-//                           style: TextStyle(
-//                             fontSize: 18,
-//                             color: blackColor,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${reverseBonus[index]}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: reverseBonus[index] < 11 ? num0Color : reverseBonus[index] < 21 ? num10Color : reverseBonus[index] < 31 ? num20Color  : reverseBonus[index] < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// // ------------------------------------- 당첨번호 순위 -------------------------------------
-// class DuplicateNumsScreen extends StatelessWidget {
-//   const DuplicateNumsScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     Map<int, int> countMap = {};
-
-//     for (int value in allHistoryNumbers) {
-//       countMap[value] = (countMap[value] ?? 0) + 1;
-//     }
-
-//     List<MapEntry<int, int>> sortedEntries = countMap.entries.toList()
-//       ..sort((a, b) => b.value.compareTo(a.value));
-      
-//     return Container(
-//       padding: const EdgeInsets.symmetric(horizontal: 16),
-//       width: MediaQuery.of(context).size.width,
-//       height: MediaQuery.of(context).size.height,
-//       child: Center(
-//         child: ConstrainedBox(
-//           constraints: const BoxConstraints(
-//             maxWidth: 500,
-//           ),
-//           child: ListView.builder(
-//             itemCount: 45,
-//             itemBuilder: (context, index){
-//               return Container(
-//                 height: 56,
-//                 // margin: const EdgeInsets.symmetric(vertical: 8),
-//                 decoration: BoxDecoration(
-//                   border: Border(
-//                     bottom: BorderSide(
-//                       width: 1,
-//                       color: greyColor,
-//                     )
-//                   )
-//                 ),
-//                 child: Row(
-//                   children: [
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(horizontal: 4),
-//                       child: Container(
-//                         padding: const EdgeInsets.only(right: 8),
-//                         width: 48,
-//                         decoration: BoxDecoration(
-//                           border: Border(
-//                             right: BorderSide(
-//                               width: 2,
-//                               color: greyColor,
-//                             )
-//                           )
-//                         ),
-//                         child: Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: [
-//                             Text(
-//                               '${index + 1}',
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 color: blackColor,
-//                               ),
-//                             ),
-//                             Text(
-//                               '위',
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 color: blackColor,
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${sortedEntries[index].key}',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             // color: blackColor,
-//                             color: sortedEntries[index].key < 11 ? num0Color : sortedEntries[index].key < 21 ? num10Color : sortedEntries[index].key < 31 ? num20Color  : sortedEntries[index].key < 41 ? num30Color : num40Color,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       flex: 1,
-//                       child: Center(
-//                         child: Text(
-//                           '${sortedEntries[index].value} 회',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             color: blackColor,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// void test() {
-//   Map<int, int> countMap = {};
-//   for (int value in allHistoryNumbers) {
-//     countMap[value] = (countMap[value] ?? 0) + 1;
-//   }
-//   List<MapEntry<int, int>> sortedEntries = countMap.entries.toList()
-//     ..sort((a, b) => b.value.compareTo(a.value));
-//   int count = 0;
-//   for (MapEntry<int, int> entry in sortedEntries) {
-//     if (count < 10) {
-//       print('${entry.key}: ${entry.value}번');
-//       count++;
-//     } else {
-//       break;
-//     }
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$number',
+      style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        color: getTextColor(number),
+      ),
+    );
+  }
+}
